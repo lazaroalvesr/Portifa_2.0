20a031d0bc316fc51a7ba27b710988ef5d17dad0
lazaro <lazaroalves12355@gmail.com>
2024-07-26 15:25:25 -0300

    v0.0.01

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="shortcut icon" href="/icons/FaviIcon.png" type="image/png" />
+        <link rel="shortcut icon" href="/icon/FaviIcon.png" type="image/png" />
       </head>
 
       <body className={PoppinsFont.className}>
